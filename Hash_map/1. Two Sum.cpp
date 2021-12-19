ea2d4ac14138bdb75d9e2899a8e4ac52efefe159
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2021-12-19 10:05:39 +0530

    Create 1. Two Sum.cpp

---
+++ Hash_map/1. Two Sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+       unordered_map<int,int> mp;
+        vector<int> v;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.count(target-nums[i]))
+            {
+                v.push_back(i);
+                v.push_back(mp[target-nums[i]]);
+            } 
+            mp[nums[i]]=i;
+        } 
+        
+        return v;
+        
+        
+    }
+};
