e27d4cef74c0ddc186042a00a86760e43d496165
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2022-01-15 12:03:52 +0530

    Update 435. Non-overlapping Intervals.cpp

--- Intervals/435. Non-overlapping Intervals.cpp
+++ Intervals/435. Non-overlapping Intervals.cpp
@@ -10,7 +10,56 @@ Input: intervals = [[1,2],[1,2],[1,2]]
 Output: 2
 Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
 
-  
+  // Case 1:-
+// ------------------   -----------------
+// |   Interval 1   |   |   Interval 2  |
+// ------------------   -----------------
+
+//Case 2:-
+// ------------------
+// |   Interval 1   |
+// ------------------
+//            ------------------
+//            |    Interval 2  |
+//            ------------------
+
+//Case 3:-
+//  --------------------
+//  |    Interval 1    |
+//  --------------------
+//     --------------
+//     | Interval 2 |
+//     --------------
+
+class Solution 
+{
+public:
+    int eraseOverlapIntervals(vector<vector<int>>&v) 
+    {
+        sort(v.begin(),v.end());
+        int count=0,l=0,r=1,n=v.size();
+        while(r<n)
+        {
+            if(v[l][1]<=v[r][0]) // Non-overlapping case(Case 1)
+            {
+                l=r;
+                r++;
+            }
+            else if(v[l][1]<=v[r][1])  // Case 2
+            {
+                count++;
+                r++;
+            }
+            else if(v[l][1]>v[r][1])   // Case 3
+            {
+                count++;
+                l=r;
+                r++;
+            }
+        }
+        return count;
+    }
+};
   class Solution {
 public:
     
