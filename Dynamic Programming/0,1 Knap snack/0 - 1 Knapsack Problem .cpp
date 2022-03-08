53193ba108e8659b25854bb81efff53a77ad887d
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2022-03-08 08:00:35 +0530

    Update 0 - 1 Knapsack Problem .cpp

--- Dynamic Programming/0,1 Knap snack/0 - 1 Knapsack Problem .cpp
+++ Dynamic Programming/0,1 Knap snack/0 - 1 Knapsack Problem .cpp
@@ -53,3 +53,44 @@ int dp[1002][1002];
        
     }
   
+// iterative solution
+
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int w, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       
+       int dp[1002][1002];
+       
+       for(int i=0;i<n+1;i++)
+       {
+           for(int j=0;j<w+1;j++)
+           {
+               if(i==0 or j==0)
+                dp[i][j]=0;
+           }
+       } 
+       
+       
+       for(int i=1;i<n+1;i++)
+       {
+           for(int j=1;j<w+1;j++)
+           {
+               if(wt[i-1]<=j)
+               {
+                   dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+               } 
+               else 
+               {
+                   dp[i][j]=dp[i-1][j];
+               }
+           }
+       } 
+       
+       return dp[n][w];
+       
+    }
+};
