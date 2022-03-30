2730ffb9ce66622ef01c266b3cecbbfa9c7e7eed
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2022-03-30 15:22:41 +0530

    Update 1143. Longest Common Subsequence.cpp

--- Dynamic Programming/string/1143. Longest Common Subsequence.cpp
+++ Dynamic Programming/string/1143. Longest Common Subsequence.cpp
@@ -31,3 +31,19 @@ public:
         return solve(m-1,n-1,text1,text2,dp);
     }
 };
+
+
+vector<vector<int> > dp(1001,vector<int> (1001,0));
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else 
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            }
+        } 
+        
+        return dp[m][n];
