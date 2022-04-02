097ec910dd781549b28bb63c96a1ab06ac64c549
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2022-04-02 20:02:04 +0530

    Create 121. Best Time to Buy and Sell Stock.cpp

---
+++ Dynamic Programming/stocks/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {     
+        int mini=INT_MAX,p=0;
+        for(int i=0;i<prices.size();i++)
+        {
+          
+               mini=min(mini,prices[i]);
+               p=max(p,prices[i]-mini);
+        } 
+        
+        return p;
+    }
+};
