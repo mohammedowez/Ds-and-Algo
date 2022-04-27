53b34ba8d60d034227c293387153ae30e932ff28
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2022-04-27 10:28:45 +0530

    Create Detect cycle in an undirected graph .cpp

---
+++ Graph/Detect cycle in an undirected graph .cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+   bool cycle(int node, int parent, vector<int>& vis, vector<int> adj[])
+   {
+     vis[node]=1;
+     for(auto it: adj[node])
+     {
+         if(!vis[it]){
+             cycle(it,node,vis,adj);
+         }
+         else if(it != parent)
+            return 1;
+     }
+     return 0;
+ }
+ 
+   // Function to detect cycle in an undirected graph.
+   bool isCycle(int V, vector<int> adj[]) 
+   {
+       // Code here
+       vector<int> vis(V+1,0);
+       for(int i=1;i<V;++i)
+       {
+           if(!vis[i])
+           {
+               if(cycle(i,-1,vis,adj))return true;
+           }
+       }
+       return 0;
+   }
+};
